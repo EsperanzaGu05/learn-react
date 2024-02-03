1bc9b5cd96e095368547cd0cbfaeb2dc335312f1
Esperanza Gualdron <espeeranza.g.p@gmail.com>
2024-02-03 17:29:03 -0500

    Twitter feature

---
+++ projects/00-hello-world/src/App.jsx
@@ -0,0 +1,16 @@
+export function App() {
+  return (
+    <article>
+      <header>
+        <img alt="Avatar" src="./src/images/winter.jpg"></img>
+        <div>
+          <strong>Esperanza Gualdron</strong>
+          <span>@EsperanzaGu05</span>
+        </div>
+      </header>
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  );
+}
